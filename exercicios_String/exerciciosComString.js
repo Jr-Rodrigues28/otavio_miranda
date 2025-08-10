c92492edb93c8920037aa9e6c6cac2fc05554008
Rodrigues <7jsdrcj@gmail.com>
2025-08-09 23:51:57 -0300

    Usando as propriedades da String

---
+++ exercicios_String/exerciciosComString.js
@@ -0,0 +1,26 @@
+// const nome = prompt('Digite seu nome completo:');
+
+//     let resultado = '';
+//     resultado += `Seu nome é: <strong>${nome}</strong><br />`;
+//     resultado += `Seu nome tem <strong>${nome.length}</strong> letras <br />`;
+//     resultado += `A segunda letra do seu nome é: <strong>${nome[1]}</strong><br />`;
+//     resultado += `Qual o primeiro índice da letra "a" no seu nome? <strong>${nome.indexOf('a')}</strong><br />`;
+//     resultado += `Qual o último índice da letra "a" no seu nome? <strong>${nome.lastIndexOf('a')}</strong><br />`;
+//     resultado += `As últimas 3 letras do seu nome são: <strong>${nome.slice(-3)}</strong><br />`;
+//     resultado += `As palavras do seu nome são: <strong>${nome.split(' ').join(', ')}</strong><br />`;
+//     resultado += `Seu nome com letras maiúsculas: <strong>${nome.toUpperCase()}</strong><br />`;
+//     resultado += `Seu nome com letras minúsculas: <strong>${nome.toLowerCase()}</strong><br />`;
+
+//     document.body.innerHTML = resultado;
+
+
+const nome = prompt('Digite seu nome completo:');
+document.body.innerHTML += `Seu nome é: <strong>${nome}</strong><br />`;
+document.body.innerHTML += `Seu nome tem <strong>${nome.length}</strong> letras <br />`;
+document.body.innerHTML += `A segunda letra do seu nome é: <strong>${nome[1]}</strong><br />`;
+document.body.innerHTML += `Qual o primeiro índice da letra LETRA a no seu nome? <strong>${nome.indexOf('a')}</strong><br />`;
+document.body.innerHTML += `Qual o último indice da letra LETRA a no seu nome? <strong>${nome.lastIndexOf('a')}</strong><br />`;
+document.body.innerHTML += `As últimas 3 letras do seu nome são: <strong>${nome.slice(-3)}</strong><br />`; // é tipo um array
+document.body.innerHTML += `As palavras do seu nome são: <strong>${nome.split(' ')}</strong><br />`;
+document.body.innerHTML += `Seu nome com letras maiúsculas: <strong>/${nome.toUpperCase()}</strong><br />`;
+document.body.innerHTML += `Seu nome com letras minúsculas: <strong>${nome.toLowerCase()}</strong><br />`;
--- maisSobreStrings.js
+++ maisSobreStrings.js
@@ -1,3 +1,14 @@
+
+//               0123456789
 let umaString = 'Um "texto"'; // posso usar barra invertida "Um \"texto\"";
 
 console.log(umaString[5]); // [5] vai exibrir somente a letra e
+            // .charAt(8)
+            // .indexOf() = pra procurar onde algo começa
+            // .replace() = para substituir ex: ('Um', 'Outra')
+            // .repalce(/t/g, 'r') = subistitui onde tem t pelo r, caso eu coloque o g 
+            // .length() = saber o tamanho da string
+            // .slice(-3) = pega um pedaço do que vc pede o -3 vai pegar de trás pra frente
+            // .split() = pra contar ou dividir as letras ou palavras
+            // .toUpperCase() = deixa a palavra maiuscula 
+            // .toLowerCase() = deixa a palavra minuscula
